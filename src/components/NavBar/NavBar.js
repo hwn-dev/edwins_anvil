350ed91c18a7432c422a64268a17ffd522a0cca7
Harry Neale <harry.neale@vodafone.com>
2022-05-08 14:48:25 +0100

    more styling + scroll behaviour

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,20 +4,20 @@ export default function NavBar() {
     return (
         <div className="nav-bar">
             <div className="logo-section">
-                <a href="/" className="nav-bar-link">
+                <a href="#home" className="nav-bar-link">
                     <h1>Edwin's Avil</h1>
                 </a>
             </div>
             <ul>
+                <a href="#home" className="nav-bar-link">
+                    Home
+                </a>
                 <a href="#projects" className="nav-bar-link">
                     Projects
                 </a>
                 <a href="#about" className="nav-bar-link">
                     About
                 </a>
-                {/* <NavLink to="/contact" className="nav-bar-link">
-                    Contact
-                </NavLink> */}
             </ul>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import '../App.css'
 
 export default function Home() {
     return (
-        <div className="home-page">
+        <div className="home-page" id="home">
             <div className="home-page-content">
                 <p>Artist.</p>
                 <p>Creator.</p>
